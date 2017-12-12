07464a4f85ef0180a85ead84a6e000dd925b5db8
damianedwards <damian@damianedwards.com>
2017-12-12 11:20:48 -0800

    Initial commit

---
+++ VbStuff/Pages/Index.cshtml.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.AspNetCore.Mvc.RazorPages
+
+Namespace Pages
+
+    Public Class IndexModel
+        Inherits PageModel
+
+        Public Property Message() As String
+
+        Public Sub OnGet()
+            Message = "Hello from VB PageModel"
+        End Sub
+    End Class
+
+End Namespace
---
+++ VbStuff/Startup.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.AspNetCore.Builder
+Imports Microsoft.AspNetCore.Hosting
+Imports Microsoft.Extensions.DependencyInjection
+
+Public Class Startup
+    Public Sub ConfigureServices(services As IServiceCollection)
+        services.AddMvc()
+    End Sub
+
+    Public Sub Configure(app As IApplicationBuilder, env As IHostingEnvironment)
+        If (env.IsDevelopment()) Then
+            app.UseDeveloperExceptionPage()
+        End If
+
+        app.UseMvc()
+    End Sub
+End Class
